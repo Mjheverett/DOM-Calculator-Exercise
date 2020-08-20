e23cf9967e0b1f0e2519b2a1b3bcc1309456d77d
Matthew Everett <matthew@mjheverett.com>
2020-08-20 14:55:54 -0400

    removed console logs and corrected addition function to match

--- js/index.js
+++ js/index.js
@@ -11,7 +11,6 @@ numKeys.forEach(function (element) {
         event.preventDefault();
         display.innerText += element.innerHTML;
         displayArr = [...displayArr, element.innerHTML];
-        console.log("numkey", displayArr);
     })
 })
 
@@ -26,24 +25,20 @@ opKeys.forEach(function (element) {
             const newString = currentValues.substring(0,currentValues.length-1) + element.innerHTML;
             display.innerText = newString;
             displayArr[displayArr.length-1] = element.innerHTML;
-            console.log("replacing operator char", displayArr)
         } else if (currentValues.length == 0) {
             alert("Type a number first");
         } else {
             display.innerText += element.innerHTML;
             displayArr = [...displayArr, element.innerHTML];
-            console.log(displayArr);
         }
     })
 })
 
 equals.addEventListener('click', function (event) {
     event.preventDefault();
-    console.log("equal button displayArr", displayArr);
-
     let numStringHolder = "";
     let calcArr = [];
-
+    
     for (let char of displayArr) {
         const numReg = /\d/;
         if (numReg.test(char) || char === '.') {
@@ -53,9 +48,8 @@ equals.addEventListener('click', function (event) {
             numStringHolder = "";
         }
     }
-    calcArr = [...calcArr, Number(numStringHolder)];
-    console.log("The calcarr inside equals", calcArr);
 
+    calcArr = [...calcArr, Number(numStringHolder)];
     let multiplication = calcArr.indexOf('*');
     while (multiplication !== -1) {
         calcArr.splice(multiplication-1, 3, calcArr[multiplication-1] * calcArr[multiplication+1]);
@@ -70,14 +64,7 @@ equals.addEventListener('click', function (event) {
 
     let addition = calcArr.indexOf('+');
     while (addition !== -1) {
-        const addNums = [];
-        addNums.push(calcArr[addition-1], calcArr[addition+1]);
-        console.log("addnums", addNums);
-        var sum = addNums.reduce(function(a, b) {return a + b;},0);
-        console.log("sum", sum)
-        console.log("calcArr", calcArr)
-        calcArr.splice(addition-1, 3, sum);
-        console.log("after addition", calcArr);
+        calcArr.splice(addition-1, 3, calcArr[addition-1] + calcArr[addition+1]);
         addition = calcArr.indexOf('+');
     };
 
@@ -86,15 +73,11 @@ equals.addEventListener('click', function (event) {
         calcArr.splice(subtraction-1, 3, calcArr[subtraction-1] - calcArr[subtraction+1]);
         subtraction = calcArr.indexOf('-');
     };
-    
+
     displayArr = [];
     displayArr.push(calcArr);
-    console.log("display after equals", display.innerText);
     displayArr = calcArr.toString();
-    console.log("displayArr after equals", displayArr);
-    console.log("calcArr after equals", calcArr);
     display.innerText = calcArr.toString();
-    console.log(display.innerText);
 })
 
 clear.addEventListener('click', function (event) {
